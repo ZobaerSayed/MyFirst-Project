7e5a1c15fa133f741b6f5d8fc9c33123900b3dbf
Abu Sayed Muhammad Zobaer <amuham47@my.centennialcollege.ca>
2021-09-18 23:04:03 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var myVariable =10;
+console.log("my numer is: " + myVariable);
+
+for (let index = 0; index < myVariable; index++) {
+    console.log("your number is" + index);
+    
+}
